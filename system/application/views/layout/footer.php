84d528de7d6fae780f945e4c766ae2c8bc5a68d2
Nick Freear <nfreear@yahoo.co.uk>
2010-11-05 16:25:52 +0000

    Follow-on fix for issue #51, 'powered by' link

--- system/application/views/layout/footer.php
+++ system/application/views/layout/footer.php
@@ -7,7 +7,7 @@
 				        <?php $powered = t('Powered by !name, open-source social software', array('!name'=>'CloudEngine')); ?>
 				        <a href="http://getcloudengine.org/" rel="bookmark"
 				          class="poweredby" title="<?= $powered ?>"
-				          ><img src="<?= base_url()?>/_design/cloudengine-sm2.gif" alt="<?= $powered ?>" /></a>
+				          ><img src="<?= base_url()?>/_design/cloudengine-sm.gif" alt="<?= $powered ?>" /></a>
 				          </p>
 				      <?php $this->load->view('layout/language_menu.php') ?>
 				        
