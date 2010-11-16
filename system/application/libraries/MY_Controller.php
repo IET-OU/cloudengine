601afcff8c372d7996ce39f2a2b2979b2cd338f0
Richard Lovelock <rikkiola@gmail.com>
2010-11-16 11:58:50 +0000

    #59 change to MY_Controller to check for active session to fix bug when installing cloudengine

--- system/application/libraries/MY_Controller.php
+++ system/application/libraries/MY_Controller.php
@@ -8,10 +8,12 @@ class  MY_Controller  extends  Controller  {
       //get message unread count for the user, this is called on nearly every page and is used to
       //update the message count in the primary navigation for a user 
       //(most controllers extend MY_Controller)
-      if (is_numeric($this->db_session->userdata('id'))) {
-        $this->load->model('message_model');
-        $user_message_count = $this->message_model->get_user_new_message_count($this->db_session->userdata('id'));
-        $this->db_session->set_userdata('user_message_count', $user_message_count);
+      if ($this->db_session) { 
+        if (is_numeric($this->db_session->userdata('id'))) {
+          $this->load->model('message_model');
+          $user_message_count = $this->message_model->get_user_new_message_count($this->db_session->userdata('id'));
+          $this->db_session->set_userdata('user_message_count', $user_message_count);
+        }
       }
                     
   }
