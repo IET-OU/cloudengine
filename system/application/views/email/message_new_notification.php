635ac48e76624f8064e11873aad85e8df80e19ee
Nick Freear <nfreear@yahoo.co.uk>
2010-11-26 14:43:16 +0000

    Fixes issue #91, use t() function in messaging email views - @i18n.

--- system/application/views/email/message_new_notification.php
+++ system/application/views/email/message_new_notification.php
@@ -1,7 +1,10 @@
 <font style="font: normal 0.8em Helvetica">
-<p>Dear <?= $recipient->fullname ?></p>
-<p>You have received a new message on Cloudworks from  <?= $thread->author->fullname ?> titled "<?= $thread->subject ?>". </p>
-<p>Once logged in you can view the new message by following this link: <?= anchor('message/thread/' .$thread->thread_id, base_url().'message/thread/' .$thread->thread_id) ?></p>
-<p><small>You can turn off these e-mails at 
-<?= anchor('user/preferences', base_url().'user/preferences') ?></small></p>
+<p><?=t('Dear !fullname,', array('!fullname'=>$recipient->fullname)) ?></p>
+<p><?=t('You have received a new message on !site-name! from !person titled "!subject".',
+    array('!person'=>$thread->author->fullname, '!subject'=>$thread->subject)) ?></p>
+<p><?=t('Once logged in you can view the new message by following this link: !link',
+    array('!link' => anchor('message/thread/'.$message->thread_id, site_url('message/thread/' .$message->thread_id)) )) ?></p>
+<p><small><?=t('You can turn off these e-mails at !link',
+    array('!link' => anchor('user/preferences', site_url('user/preferences')) )) ?>
+</small></p>
 </font> 
