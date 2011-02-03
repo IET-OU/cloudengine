d9ca7c2d7ebc7c1147a3e6cbc4cb56b841da61b1
Richard Lovelock <rikkiola@gmail.com>
2011-02-03 12:20:18 +0000

    #135 Messaging - minor enhancements

--- system/application/views/message/list.php
+++ system/application/views/message/list.php
@@ -49,7 +49,7 @@
                       <table>
                         <tbody>
                           <tr>
-                            <td class="replied">
+                            <td class="row-click replied">
                               <?php if ($user_id == $thread->last_message_author_id): ?>
                                 <img src="<?=base_url()?>_design/replied_arrow.png" />
                               <?php endif; ?>
@@ -60,13 +60,13 @@
                             <td class="profile-pic">
                               <div class="thread-profile-pic">
                                 <?php if ($thread->picture): ?>
-                                    <img src="<?= base_url() ?>image/user/<?= $thread->last_message_author_id ?>" height="50" width="50" class="go2" alt="" />
+                                    <?=anchor("user/view/$thread->last_message_author_id", img(array('src'=>base_url().'image/user/'.$thread->last_message_author_id, 'height' => '50', 'width' => '50', 'alt' => $thread->last_message_author_name))) ?>
                                 <?php else: ?>
-                                    <img src="<?=base_url() ?>_design/avatar-default-32.jpg" class="go2 message-pic-anon" alt="" />
+                                    <?=anchor("user/view/$thread->last_message_author_id", img(array('src'=>base_url().'_design/avatar-default-32.jpg', 'alt' => $thread->last_message_author_name, 'class' => 'go2 message-pic-anon'))) ?>                                    
                                 <?php endif; ?>
                               </div>
                             </td>
-                            <td class="last-message-wrapper">
+                            <td class="row-click last-message-wrapper">
                               <div class="last-message-author message-list-top-line">
                                 <?php if($thread->last_message_author_id == $user_id): ?>
                                   <?=anchor("user/view/$thread->last_message_author_id", 'You') ?>
@@ -78,18 +78,18 @@
                                 <?= format_date('!date-time-abbr!',$thread->last_message_date) ?>
                               </div>
                             </td>       
-                            <td class="subject">
+                            <td class="row-click subject">
                               <?=anchor("message/thread/" .$thread->thread_id, $thread->subject, 'class="message-list-top-line"' ) ?>
                               <div class="preview message-list-bottom-line">
                                 <?= $thread->content_preview ?>
                               </div>
                             </td>                        
-                            <td class="participants message-list-bottom-line"></a>
+                            <td class="row-click participants message-list-bottom-line"></a>
                               <?php //this looks a bit of a mess but needed to close code to suppress white space ?>
                               <?= anchor("user/view/" .$user_id, 'You') ?><?php if ($thread->other_participant_count == 1): ?><?= ' and ' .anchor("user/view/" .$thread->other_participants[0]->user_id, trim($thread->other_participants[0]->name)) ?><?php else: ?><?php for($i=0; $i < $thread->other_participant_count; $i++): ?><?php if ($i < 3): ?><?=', '.anchor("user/view/".$thread->other_participants[$i]->user_id, trim($thread->other_participants[$i]->name)) ?><?php endif; ?><?php endfor; ?><?php if ($thread->other_participant_count > 3): ?> + <?php print($thread->other_participant_count - 3); ?> others<?php endif; ?> <?php endif; ?>
                             
                             </td>                                            
-                            <td class="message-count message-list-top-line">
+                            <td class="row-click message-count message-list-top-line">
                               <?= $thread->total_messages ?> 
                               <?php if (intval($thread->new_messages)): ?>
                                 <div class='new-messages message-list-bottom-line'> <?= $thread->new_messages ?>  new </div>
