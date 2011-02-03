7c42201077f7cef627394892a368306b0296922c
Nick Freear <nfreear@yahoo.co.uk>
2011-02-03 14:45:50 +0000

    #137, Fix main navigation - no link when URL matches current URL + #136, HTML5 validation.

--- system/application/views/layout/_site-header.php
+++ system/application/views/layout/_site-header.php
@@ -1,8 +1,8 @@
-<?php $current_page = str_replace(base_url(), '', current_url()); ?> 
+<?php $current_page = str_replace(base_url(), '/', current_url()); ?> 
 <div id="site-header-content">
         <div id="skip">
         <a href="#content"><?=t('Skip navigation') ?></a> </div>
-        <?php if ($current_page != ''): ?>
+        <?php if ($current_page != '/'): ?>
         <a rel="home" href="<?=base_url()?>">
         <?php endif; ?>
         <img id="link-home" src="<?=base_url()?><?= $this->config->item('theme_logo') ?>" alt="<?=t("!site-name! home page") ?>" />
