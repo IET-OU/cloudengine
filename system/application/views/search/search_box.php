e71368e12a9d52ec69c7f4e772cca6c3a308a1ff
Nick Freear <nfreear@yahoo.co.uk>
2011-02-03 15:02:08 +0000

    #136, Fix HTML validation errors - search_box.

--- system/application/views/search/search_box.php
+++ system/application/views/search/search_box.php
@@ -1,10 +1,10 @@
 <?php if(config_item('x_search')): ?>
 <div id="search-site">
-    <form id="form-search" action="<?=base_url() ?>search/result" method="post">
+    <form id="form-search" action="<?=site_url('search/result') ?>" method="post">
       <p>
         <label for="query_string"><?=t("Search")?></label>
         <input id="query_string" name="query_string" type="text" />
-        <input id="search-button" name="search-button" type="image" value="submit" src="<?=base_url() ?>_design/search-button.gif" alt="<?=t("Search")?>" />
+        <input id="search-button" name="search-button" type="image" src="<?=base_url() ?>_design/search-button.gif" alt="<?=t("Search")?>" />
       </p>
     </form>
 </div>
