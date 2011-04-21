8aace100fa2f20077976653efcf641e05e4a24a9
Nick Freear <nfreear@yahoo.co.uk>
2011-04-21 15:50:25 +0100

    Adding opensearch description XML.

---
+++ system/application/views/search/opensearch_description.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * OpenSearch description - cross-browser search plugin.
+ *
+ * @copyright 2009, 2010 The Open University. See CREDITS.txt
+ * @license   http://gnu.org/licenses/gpl-2.0.html GNU GPL v2
+ * @package Search
+ */
+$site_name = $this->config->item('site_name');
+header('Content-Type: application/opensearchdescription+xml; charset=utf-8');
+@header('Content-Disposition: inline; '.$_SERVER['HTTP_HOST'].'-opensearch.xml');
+?>
+<?xml version="1.0" encoding="utf-8"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/"
+                       xmlns:moz="http://www.mozilla.org/2006/browser/search/">
+  <ShortName><?=$site_name ?></ShortName>
+  <Description><?=$this->config->item('tag_line') ?></Description>
+  <Contact><?=$this->config->item('site_email') ?></Contact>
+  <Image type="image/vnd.microsoft.icon" width="16" height="16"><?=base_url(). $this->config->item('theme_favicon') ?></Image>
+  <Url type="text/html" template="<?=base_url() ?>/search/result?q={searchTerms}"/>
+  <LongName></LongName>
+  <Query role="example" searchTerms="OULDI" />
+  <Developer>CloudEngine/OULDI development team at The Open University.</Developer>
+  <Attribution>Search data Copyright 2011 <?=$site_name ?>.</Attribution>
+  <SyndicationRight>open</SyndicationRight>
+  <Language><?=$this->config->item('default_language') ?></Language>
+  <InputEncoding>UTF-8</InputEncoding>
+  <OutputEncoding>UTF-8</OutputEncoding>
+  <moz:SearchForm><?=base_url() ?>/search</moz:SearchForm>
+</OpenSearchDescription>
