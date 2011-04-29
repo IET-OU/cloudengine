29bdd60572c8116a75f93f055033213c257a763b
Nick Freear <nfreear@yahoo.co.uk>
2011-04-29 14:52:08 +0100

    #175 follow-up - Prevent warning for empty  in log.

--- system/application/views/search/search_box.php
+++ system/application/views/search/search_box.php
@@ -1,9 +1,11 @@
-<?php if(config_item('x_search')): ?>
+<?php if(config_item('x_search')):
+  $query = isset($query_string) ? $query_string : '';
+?>
 <div id="search-site">
     <form id="form-search" action="<?=site_url('search/result') ?>" method="get">
       <p>
         <label for="query"><?=t("Search")?></label>
-        <input id="query" name="q" type="search" value="<?=$query_string ?>" maxlength="30" />
+        <input id="query" name="q" type="search" value="<?=$query; ?>" maxlength="30" />
         <input id="search-button" type="image" src="<?=base_url() ?>_design/search-button.gif" alt="<?=t("Search")?>" />
       </p>
     <?php $this->load->view('search/plugin'); ?>
