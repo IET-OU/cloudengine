d31691d2fc56cecf8eb4ed44970752bdda54709b
Nick Freear <nfreear@yahoo.co.uk>
2011-04-29 14:58:49 +0100

    #149, converting to unobtrusive Javascript - search/plugin view.

--- system/application/views/search/plugin.php
+++ system/application/views/search/plugin.php
@@ -3,6 +3,7 @@
    Source: http://mycroft.mozdev.org/developer/hosting.html
    Experimental:  /search?plugin=1
    Supported on:  MSIE 7+, Firefox 2+, Chrome.
+   Todo: move CSS to stylesheet.
 */
 if ($this->input->get('plugin')):
 ?>
@@ -12,10 +13,16 @@ if ($this->input->get('plugin')):
  rel="search" type="application/opensearchdescription+xml"
  title="<?=$this->config->item('site_name') ?>"
  href="<?=site_url('search/opensearch_desc.xml') ?>" />
+
+<a role="button" id="search-plugin-btn" href="<?=site_url('search/opensearch_desc.xml') ?>" title=
+ "<?=t('Add the !site-name! search plugin to your browser') ?>" style="display:inline-block; background:#D63333; color:#eee; padding:4px; margin:4px 0; --moz-appearance:button;"
+ ><?=t('Add browser search plugin') ?></a>
+<br /><small>(Supported in Internet Explorer 7+, Firefox and Chrome.)</small>
+</p>
 <script>
-function addOpenSearch(name,ext,meth) {
+$('#search-plugin-btn').click(function addOpenSearch(name,ext,meth) {
   if ((typeof window.external == "object") && ((typeof window.external.AddSearchProvider == "unknown") || (typeof window.external.AddSearchProvider == "function"))) {
-    if ((typeof window.external.AddSearchProvider == "unknown") && meth == "p") {
+    if((typeof window.external.AddSearchProvider == "unknown") && meth == "p"){
       alert("This plugin uses POST which is not currently supported by Internet Explorer's implementation of OpenSearch.");
     } else {
       window.external.AddSearchProvider(
@@ -26,11 +33,6 @@ function addOpenSearch(name,ext,meth) {
     alert("<?=t('You will need a browser which supports OpenSearch to install this plugin.') ?>");
   }
   return false;
-}
+});
 </script>
-<a role="button" onclick="return addOpenSearch();" href="<?=site_url('search/opensearch_desc.xml') ?>" title=
- "<?=t('Add the !site-name! search plugin to your browser') ?>" style="display:inline-block; background:#D63333; color:#eee; padding:4px; margin:4px 0; --moz-appearance:button;"
- ><?=t('Add browser search plugin') ?></a>
-<br /><small>(Supported in Internet Explorer 7+, Firefox and Chrome.)</small>
-</p>
 <? endif; ?>
