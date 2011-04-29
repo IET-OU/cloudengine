cd4093c3f12f577e36f8636bec952298d0586f15
Nick Freear <nfreear@yahoo.co.uk>
2011-04-29 09:23:13 +0100

    (#175) Tidying search forms.

--- system/application/views/search/search_box.php
+++ system/application/views/search/search_box.php
@@ -2,9 +2,9 @@
 <div id="search-site">
     <form id="form-search" action="<?=site_url('search/result') ?>" method="get">
       <p>
-        <label for="query_string"><?=t("Search")?></label>
-        <input id="query_string" name="q" type="search" value="<?=$query_string ?>" maxlength="30" />
-        <input id="search-button" name="search-button" type="image" src="<?=base_url() ?>_design/search-button.gif" alt="<?=t("Search")?>" />
+        <label for="query"><?=t("Search")?></label>
+        <input id="query" name="q" type="search" value="<?=$query_string ?>" maxlength="30" />
+        <input id="search-button" type="image" src="<?=base_url() ?>_design/search-button.gif" alt="<?=t("Search")?>" />
       </p>
     </form>
 </div>
--- system/application/views/search/search_form.php
+++ system/application/views/search/search_form.php
@@ -1,9 +1,9 @@
 <?php if(config_item('x_search')): ?>
 <form id="form-search" action="<?=site_url('search/result') ?>" method="get">
   <p>
-    <label for="query_string"><?=t("Search")?></label>
-    <input id="query_string" name="q" type="search" size="50" maxlength="30" />
-    <button type="submit" id="search-submit" value="Search"><?=t("Search")?></button>
+    <label for="query"><?=t("Search")?></label>
+    <input id="query" name="q" type="search" size="50" maxlength="30" />
+    <button type="submit" id="search-submit"><?=t("Search")?></button>
   </p>
 </form>
 <?php endif; ?>
