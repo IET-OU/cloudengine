7054d03931c89f51e0a718b8ebea0a3cc794e4b0
Juliette Culver <jvvw@jvvw.com>
2012-10-17 14:46:14 +0100

    #219

--- system/application/views/badge/application_received.php
+++ system/application/views/badge/application_received.php
@@ -1,7 +1,7 @@
 <div id="region1">
 <h1><?=t("Application recieved")?></h1>
 <p><?=t("Your application for the badge !name has been received. You will receive email 
-notification when it has been approved or rejected.",
+notification when it has been approved.",
     array('!name' => anchor("badge/view/$badge->badge_id", $badge->name)))?></p>
     
     <p><?= anchor('badge/badge_list', t("Back to list of badges")) ?></p>
