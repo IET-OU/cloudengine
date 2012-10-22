f62f2f989d5c2017d102373ac6d37ce41ef4feee
Juliette Culver <jvvw@jvvw.com>
2012-10-22 11:24:25 +0100

    #219

--- system/application/views/badge/issue.php
+++ system/application/views/badge/issue.php
@@ -1,7 +1,9 @@
 <script src="http://beta.openbadges.org/issuer.js"></script>
 <script type="text/javascript">
 OpenBadges.issue(['<?= base_url() ?>/badge/assertion/<?= $application_id ?>'], 
-function(errors, successes) { console.log(errors); console.log(successes); })
-
+function(errors, successes) { 
+    console.log(errors); 
+    console.log(successes); 
+});
 </script>
 <p>Badge issued</p>
