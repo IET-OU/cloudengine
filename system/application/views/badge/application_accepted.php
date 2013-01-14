0063100afda8a05fd01a8e6eab2d1722fcf7e015
Juliette Culver <jvvw@jvvw.com>
2013-01-14 09:39:59 +0000

    Removing unused files

--- system/application/views/badge/application_accepted.php
+++
@@ -1,9 +0,0 @@
-<div id="region1">
-<h1><?=t("Application recieved")?></h1>
-<p><?=t("Your application for the badge !name has been received. You will receive email 
-notification when it has been approved or rejected.",
-    array('!name' => anchor("badge/view/$badge->badge_id", $badge->name)))?></p>
-    
-    <p><?= anchor('badge/badge_list', t("Back to list of badges")) ?></p>
-
-</div>
