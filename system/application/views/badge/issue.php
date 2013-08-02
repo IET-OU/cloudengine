229a882e9c7c8d4b12267abf21d14be8595fa5c9
Nick Freear <nfreear@yahoo.co.uk>
2013-08-02 16:34:30 +0100

    Bug #347, Further work on Javascript - 'badge/issue' view
    * Improve feedback to user - Mozilla Open Badges
    * https://github.com/mozilla/openbadges

--- system/application/views/badge/issue.php
+++ system/application/views/badge/issue.php
@@ -4,17 +4,40 @@
 <script src="http://beta.openbadges.org/issuer.js"></script>
 <script>
 (function () {
-  var p = document.querySelector("#badge-issue-stat");
+  'use strict';
+
+  var p = document.querySelector("#badge-issue-stat"),
+    // https://github.com/mozilla/openbadges/wiki/Issuer-API#error-constant-strings
+    ers = {
+      "DENIED":  "User cancelled",
+      "EXISTS":  "Badge already exists",
+      "INACCESSIBLE": "Assertion URL can not be retrieved. Maybe 404 'Not Found'",
+      "MALFORMED": "Assertion URL is malformed",
+      "INVALID": "Assertion URL is not valid. Maybe not logged in"
+    };
+
   try {
-    OpenBadges.issue(['<?= base_url() ?>/badge/assertion/<?= $application_id ?>'], 
-    function (errors, successes) { 
+    OpenBadges.issue(['<?= site_url('badge/assertion/' . $application_id) ?>'],
+    function (errors, successes) {
+      var msg, cls, reason, rsn_tx;
+
       if (errors.length > 0) {
-        p.innerHTML = "Error, badge not issued.  <small>[ " + errors[0].reason + " ]</small>";
-        p.className = "error";
+        reason = errors[0].reason;
+        rsn_tx = ers[reason];
+        if ("DENIED" === reason) {
+          msg = "Warning, badge not issued. <small>[ " + rsn_tx +" - "+ reason + " ]</small>";
+          cls = "warn";
+        } else {
+          msg = "Error, badge not issued. <small>[ " + rsn_tx +" - "+ reason + " ]</small>";
+          cls = "error";
+        }
       } else {
-        p.innerHTML = "Success, badge issued OK.";
-        p.className = "ok";
+        msg = "Success, badge issued OK.";
+        cls = "ok";
       }
+      p.innerHTML = msg;
+      p.className = cls;
+
       console.log(errors);
       console.log(successes);
     });
