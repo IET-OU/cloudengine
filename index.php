e40930f6ff6c3e02c0d2583c3bd5cd4d9aa41d26
root <root@artemis.open.ac.uk>
2016-06-09 10:16:55 +0000

    Added display of errors for development environment

--- index.php
+++ index.php
@@ -10,8 +10,43 @@
 |
 */
 
-ini_set("display_errors", 'Off');
-error_reporting(0);
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ */
+define('ENVIRONMENT', 'production');
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ */
+if (defined('ENVIRONMENT'))
+{
+    switch (ENVIRONMENT)
+    {
+        case 'development':
+            // Report all errors
+            error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+
+            // Display errors in output
+            ini_set('display_errors', 1);
+        break;
+
+        case 'testing':
+        case 'production':
+            // Report all errors except E_NOTICE
+            // This is the default value set in php.ini
+            error_reporting(E_ALL & ~E_NOTICE);
+
+            // Don't display errors (they can still be logged)
+            ini_set('display_errors', 0);
+        break;
+
+        default:
+            exit('The application environment is not set correctly.');
+    }
+}
 
 /*
 |---------------------------------------------------------------
