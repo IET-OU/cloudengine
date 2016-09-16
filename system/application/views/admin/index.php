4db57c89b808592ee0b4e5c701ca78de1e45686e
IET-OU <iet-webmaster+@+open.ac.uk>
2016-09-16 13:10:26 +0000

    Reorganised admin page more sensibly

--- system/application/views/admin/index.php
+++ system/application/views/admin/index.php
@@ -1,22 +1,29 @@
 <h1><?= t("Admin Panel") ?></h1>
-<h2><?= t("Site Content") ?></h2>
+<h2><?= t("Spam Prevention") ?></h2>
 <ul class="arrows">
-<li><?= anchor('admin/moderate', t("Moderate clouds, cloudscapes and comments")) ?></li>
+<li><?= anchor('admin/moderate', t("Moderation Queue (items flagged for moderation)")) ?></li>
+<?php if ($this->config->item('x_flag')): ?>
+<li><?= anchor('admin/flagged', t("Items flagged as spam by users")) ?></li>
+<li><?= anchor('event/admin', t("Admin Cloudstream (check occasionally for spam)")) ?></li>
+<?php endif; ?>
+</ul>
+
+<h2><?= t("User Management") ?></h2>
+<ul class="arrows">
+<li><?= anchor('user/people', t("Users")) ?></li>
+<li><?= anchor('admin/unactivated_users', t("Unactivated Users")) ?></li>
+</ul>
+
+<h2><?= t("Content Management") ?></h2>
+<ul class="arrows">
 <li><?= anchor('blog/add', t("Create new blog post")) ?></li>
 <li><?= anchor('admin/featured_cloudscapes', t("Manage featured cloudscapes")) ?></li>
 <li><?= anchor('admin/update_site_news', t("Update Site News")) ?></li>
 <li><?= anchor('admin/manage_pages', t("Manage Pages")) ?></li>
-<?php if ($this->config->item('x_flag')): ?>
-<li><?= anchor('admin/flagged', t("Items flagged as spam")) ?></li>
-<?php endif; ?>
 </ul>
 
-
-<h2><?= t("Statistics and Admin Data") ?> </h2>
+<h2><?= t("Statistics") ?> </h2>
 <ul class="arrows">
-<li><?= anchor('user/people', t("Users")) ?></li>
-<li><?= anchor('admin/unactivated_users', t("Unactivated Users")) ?></li>
-<li><?= anchor('event/admin', t("Admin Cloudstream")) ?></li>
 <li><?= anchor('statistics/stats', t("Site Statistics")) ?></li>
 <li><?= anchor('statistics/stats_dates', t("Site Statistics By Date")) ?></li>
 <li><?= anchor('statistics/user_stats', t("Site User Statistics (takes a while)")) ?></li>
@@ -32,4 +39,4 @@
 <li><?= anchor('admin/recalculate_popular', t("Recalculate Popular Clouds and Cloudscapes")) ?></li>
 <li><?= anchor('admin/site_settings', t('Site settings and status')) ?></li>
 <li><?= anchor('admin/phpinfo', t('Display PHP configuration info')) ?></li>
-</ul>
+</ul
