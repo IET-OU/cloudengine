f40d4534ac03f9ae319576c1bda9ffdbbe02035c
IET-OU <iet-webmaster+@+open.ac.uk>
2016-09-16 09:56:55 +0000

    Added Ban User ban to moderation page

--- system/application/views/admin/moderate.php
+++ system/application/views/admin/moderate.php
@@ -9,18 +9,14 @@
         <p>Cloud created  by <a href="<?= base_url() ?>user/view/<?= $cloud->user_id ?>"><?= $cloud->fullname ?></a></p>
         <p><?= $cloud->summary ?></p>
         <?= $cloud->body ?>
-        
+        <p class="login">
+        <?=anchor("user/ban/$cloud->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'cloud-approve'))?>
             <input type="hidden" id="type" name="type" value="cloud" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$cloud->cloud_id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'cloud-spam'))?>
-            <input type="hidden" id="type" name="type" value="cloud" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$cloud->cloud_id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
@@ -32,18 +28,14 @@
     <?php foreach ($comments as $comment): ?>
         <p>Comment  by <a href="<?= base_url() ?>user/view/<?= $comment->user_id ?>"><?= $comment->fullname ?></a></p>
         <?= $comment->body ?>
-        
+        <p class="login">
+        <?=anchor("user/ban/$comment->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'comment-approve'))?>
             <input type="hidden" id="type" name="type" value="comment" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$comment->comment_id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'comment-spam'))?>
-            <input type="hidden" id="type" name="type" value="comment" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$comment->comment_id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
@@ -57,18 +49,14 @@
         <p>Cloudscape created  by <a href="<?= base_url() ?>user/view/<?= $cloudscape->user_id ?>"><?= $cloudscape->fullname ?></a></p>
         <p><?= $cloudscape->summary ?></p>
         <?= $cloudscape->body ?>
-        
+        <p class="login">
+        <?=anchor("user/ban/$cloudscape->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'cloudscape-approve'))?>
             <input type="hidden" id="type" name="type" value="cloudscape" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$cloudscape->cloudscape_id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'cloudscape-spam'))?>
-            <input type="hidden" id="type" name="type" value="cloudscape" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$cloudscape->cloudscape_id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
@@ -78,18 +66,14 @@
     <?php foreach ($news_comments as $comment): ?>
         <p>Comment  by <?= anchor('user/view'.$comment->user_id, $comment->fullname) ?></p>
         <?= $comment->body ?>
-        
+        <p class="login">
+        <?=anchor("user/ban/$comment->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'news_comment-approve'))?>
             <input type="hidden" id="type" name="type" value="news_comment" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$comment->comment_id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'news_comment-spam'))?>
-            <input type="hidden" id="type" name="type" value="news_comment" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$comment->comment_id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
@@ -98,19 +82,16 @@
 <?php if ($links): ?>
 <h2>Links</h2>
     <?php foreach ($links as $link): ?>
-        <p><?= $link->title ?> <?= $link->url ?> by 
+        <p><?= $link->title ?> <?= $link->url ?> by
         <?= anchor('user/view/'.$link->user_id, $link->fullname )?></p>
+        <p class="login">
+        <?=anchor("user/ban/$link->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'link-approve'))?>
             <input type="hidden" id="type" name="type" value="link" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$link->link_id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'link-spam'))?>
-            <input type="hidden" id="type" name="type" value="link" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$link->link_id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
@@ -119,19 +100,16 @@
 <?php if ($references): ?>
 <h2>References</h2>
     <?php foreach ($references as $reference): ?>
-        <p><?= $reference->reference_text ?>  by 
+        <p><?= $reference->reference_text ?>  by
         <?= anchor('user/view/'.$reference->user_id, $reference->fullname) ?></p>
+        <p class="login">
+        <?=anchor("user/ban/$reference->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'reference-approve'))?>
             <input type="hidden" id="type" name="type" value="reference" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$reference->reference_id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'reference-spam'))?>
-            <input type="hidden" id="type" name="type" value="reference" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$reference->reference_id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
@@ -141,17 +119,14 @@
 <h2>Content</h2>
     <?php foreach ($contents as $content): ?>
         <p><?= $content->body ?>  by  <?= anchor('user/view/'.$content->user_id, $content->fullname) ?></p>
+        <p class="login">
+        <?=anchor("user/ban/$content->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'content-approve'))?>
             <input type="hidden" id="type" name="type" value="content" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$content->content_id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'content-spam'))?>
-            <input type="hidden" id="type" name="type" value="content" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$content->content_id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
@@ -160,19 +135,16 @@
 <?php if ($embeds): ?>
 <h2>Embeds</h2>
     <?php foreach ($embeds as $embed): ?>
-        <p><?= $embed->title ?>  <a href="<?= $embed->url ?>"><?= $embed->url ?></a> 
+        <p><?= $embed->title ?>  <a href="<?= $embed->url ?>"><?= $embed->url ?></a>
         by  <?= anchor('user/view/'.$embed->user_id, $embed->fullname) ?></p>
+        <p class="login">
+        <?=anchor("user/ban/$embed->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'embed-approve'))?>
             <input type="hidden" id="type" name="type" value="embed" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$embed->embed_id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'embed-spam'))?>
-            <input type="hidden" id="type" name="type" value="embed" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$embed->embed_id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
@@ -180,19 +152,15 @@
 <?php if ($profiles): ?>
 <h2>Profiles</h2>
     <?php foreach ($profiles as $profile): ?>
-        <p><?= $profile->description ?>  by  <?= anchor('user/view/'.$profile->user_id, $profile->fullname) ?></p>
-
+        <p><?= $profile->description ?>  <?= $profile->homepage ?> by  <?= anchor('user/view/'.$profile->user_id, $profile->fullname) ?></p>
+        <p class="login">
+        <?=anchor("user/ban/$profile->user_id", t('Ban User'), array('title'=>t('Ban User'), 'class'=>'button')) ?>
+      </p>
         <?=form_open($this->uri->uri_string(), array('id' => 'profile-approve'))?>
             <input type="hidden" id="type" name="type" value="profile" ?>
             <input type="hidden" id="action" name="action" value="approve" ?>
-            <input type="hidden" id="id" name="id" value="<?=$profile->id ?>" ?>      
-            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
-        <?=form_close()?>
-        <?=form_open($this->uri->uri_string(), array('id' => 'profile-spam'))?>
-            <input type="hidden" id="type" name="type" value="profile" ?>
-            <input type="hidden" id="action" name="action" value="spam" ?>
             <input type="hidden" id="id" name="id" value="<?=$profile->id ?>" ?>
-            <input type="submit" name="submit" id="submit" class="submit" value="Delete" />
+            <input type="submit" name="submit" id="submit" class="submit" value="Approve" />
         <?=form_close()?>
     <?php endforeach; ?>
 <?php endif; ?>
