f89cdfc0405275eddd351b02b20608c326857b64
IET-OU <iet-webmaster+@+open.ac.uk>
2016-09-30 13:00:05 +0000

    Fixed a few bugs in the item model relating links causing the page showing items flagged as spam to error

--- system/application/models/item_model.php
+++ system/application/models/item_model.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 /**
- * Generic functions for items in the site 
- * The current item types are cloud, cloudscape and user 
- * 
+ * Generic functions for items in the site
+ * The current item types are cloud, cloudscape and user
+ *
  * @copyright 2009, 2010 The Open University. See CREDITS.txt
  * @license   http://gnu.org/licenses/gpl-2.0.html GNU GPL v2
- * @package Item 
+ * @package Item
  */
 class Item_model extends Model {
 
@@ -15,7 +15,7 @@ class Item_model extends Model {
 		$this->ci = & get_instance();
         parent::Model();
     }
-    
+
     /**
      * Determine the URL for viewing an item on the site
      *
@@ -26,42 +26,42 @@ class Item_model extends Model {
     function view($item_type, $item_id) {
        $url = FALSE;
        switch ($item_type) {
-           case 'cloud': 
-               $url = 'cloud/view/'.$item_id; 
+           case 'cloud':
+               $url = 'cloud/view/'.$item_id;
                break;
-           case 'cloudscape': 
-               $url = 'cloudscape/view/'.$item_id; 
+           case 'cloudscape':
+               $url = 'cloudscape/view/'.$item_id;
                break;
-           case 'user': 
-               $url = 'user/view/'.$item_id; 
+           case 'user':
+               $url = 'user/view/'.$item_id;
                break;
 		   case 'cloud_comment':
 			  $this->ci->load->model('comment_model');
 			  $comment = $this->ci->comment_model->get_comment($item_id);
-			  $url = 'cloud/view/'.$comment->cloud_id.'/comments#cloud_comment-'.$item_id; 
+			  $url = 'cloud/view/'.$comment->cloud_id.'/comments#cloud_comment-'.$item_id;
 			  break;
 		   case 'embed':
 			  $this->ci->load->model('embed_model');
 			  $embed = $this->ci->embed_model->get_embed($item_id);
-			  $url = 'cloud/view/'.$embed->cloud_id.'#embedt-'.$item_id; 
+			  $url = 'cloud/view/'.$embed->cloud_id.'#embedt-'.$item_id;
 			  break;
 		   case 'link':
 		      $this->ci->load->model('link_model');
-			  $link = $this->ci->link_model->get_embed($link_id);
-			  $url = 'cloud/view/'.$link->cloud_id.'/links#link-'.$item_id; 
-			  break;		   
+			  $link = $this->ci->link_model->get_link($item_id);
+			  $url = 'cloud/view/'.$link->cloud_id.'#link-'.$item_id;
+			  break;
 		   case 'content':
 		      $this->ci->load->model('content_model');
 		   	  $content = $this->ci->content_model->get_content($item_id);
-			  $url = 'cloud/view/'.$content->cloud_id.'#content-'.$item_id; 
+			  $url = 'cloud/view/'.$content->cloud_id.'#content-'.$item_id;
 			  break;
-       } 
-       return $url; 
+       }
+       return $url;
     }
-	    
+
     /**
      * Determine if a user has edit permission for an item and if not redirect to
-     * an error page. 
+     * an error page.
      *
      * @param integer $user_id The ID of the user
      * @param string $item_type The type of item e.g. 'cloud', 'cloudscape' or 'user'
@@ -69,22 +69,22 @@ class Item_model extends Model {
      */
     function check_edit_permission($user_id, $item_type, $item_id) {
        switch ($item_type) {
-           case 'cloud' : 
+           case 'cloud' :
                 $this->ci->load->model('cloud_model');
                 $this->ci->cloud_model->check_edit_permission($user_id, $item_id);
                 break;
-           case 'cloudscape' : 
+           case 'cloudscape' :
                 $this->ci->load->model('cloudscape_model');
                 $this->ci->cloudscape_model->check_admin_permission($cloudscape_id, $user_id);
                 break;
-           case 'user': 
+           case 'user':
                if ($user_id != $item_id) {
                    show_error('You do not have permission to edit the profile of this person.');
                }
         }
-    
+
     }
-    
+
     /**
      * Get the title of an item
      *
