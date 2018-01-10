b96f58ef520f848b02d0e27c0182aeea120e8b70
Nick Freear <nfreear@yahoo.co.uk>
2018-01-10 17:19:31 +0000

    Bug #374, add CodeIgniter `log_message()` to Recaptcha [iet:10288012]

--- system/application/libraries/Recaptcha.php
+++ system/application/libraries/Recaptcha.php
@@ -7,13 +7,19 @@
  * @author  Nick Freear, 2018-01-09.
  *
  * @link    https://gist.github.com/nfreear/8f74ab520daf8e42b289b04af9aa4f87
- * @link    https://developers.google.com/recaptcha/docs/verify
+ * @link    https://developers.google.com/recaptcha/
  */
 
 class Recaptcha {
 
     const URL = 'https://www.google.com/recaptcha/api/siteverify';
 
+    /** Server-side verification via the reCAPTCHA service.
+     *
+     * @param string $recap_response  The value of the 'g-recaptcha-response' POST field.
+     * @return object  A stdClass object containing 'success', 'error-codes', etc.
+     * @link  https://developers.google.com/recaptcha/docs/verify#api-response
+     */
     public static function verify( $recap_response = null ) {
 
         $recap_response = $recap_response ? $recap_response : filter_input( INPUT_POST, 'g-recaptcha-response' );
@@ -24,13 +30,16 @@ class Recaptcha {
              'response' => $recap_response,
              // 'remoteip' => ?
         ]) );
+
         $srv_response = $raw_response ? json_decode( $raw_response ) : $raw_response;
 
-        if ( ! $srv_response || ! isset( $srv_response->success ) ) {
+        if ( $srv_response && isset( $srv_response->success ) ) {
+            log_message('debug', 'reCAPTCHA: ' . json_encode( $srv_response ));
+            header( 'X-recaptcha-verify: ' . json_encode([ 'rc_resp' => $recap_response, 'srv_resp' => $srv_response ]));
+        } else {
+            log_message('error', 'reCAPTCHA: ' . json_encode([ $srv_response, $http_response_header ]));
             header( 'X-recaptcha-v-error: ' . json_encode([ $srv_response, $http_response_header ]));
         }
-
-        header( 'X-recaptcha-verify: ' . json_encode([ 'rc_resp' => $recap_response, 'srv_resp' => $srv_response ]));
 
         return $srv_response;
     }
@@ -41,13 +50,14 @@ class Recaptcha {
         return stream_context_create([
             'http' => [
                 'method' => 'POST',
-                'user_agent' => 'CloudEngine/1.0-beta +https://github.com/nfreear',
+                'user_agent' => 'CloudEngine/1.4 +https://github.com/IET-OU/cloudengine',
                 'proxy' => config_item( 'http_proxy' ),
                 'header' => [
                     'Content-Type: application/x-www-form-urlencoded',
                     'Content-Length: ' . strlen( $postdata ),
                 ],
                 'content' => $postdata,
+                'timeout' => 5, // Seconds.
             ]
         ]);
     }
